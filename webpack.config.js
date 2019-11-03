ca147ca9c7a8e23be31135dd4d12dccbde061d04
游天琪 <autumnyout@gmail.com>
2019-11-03 13:40:50 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
     app: './src/index.ts',
     // promise:'./src/promise-in-order.ts'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
   },
   module: {
     rules: [
@@ -24,7 +28,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
-    new HtmlWebpackPlugin(),
+    new HtmlWebpackPlugin({title: 'Development',}),
     new CleanWebpackPlugin()
   ]
 };
